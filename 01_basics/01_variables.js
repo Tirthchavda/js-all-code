f0785705d7b2d646179caa9a74668b3b7aa35287
Tirthchavda <tirth7859@gmail.com>
2024-12-10 23:49:24 +0530

    hi

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144334
+let accountEmail = "tirth24@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mehsana"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail="tc@tc.com"
+accountPassword="3467"
+accountCity="goa"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
