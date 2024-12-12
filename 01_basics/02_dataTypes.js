a8387d24e61b80df825da9c4875f0511cab82436
Tirthchavda <tirth7859@gmail.com>
2024-12-12 23:47:25 +0530

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,4 +3,23 @@
 // alert(3+3)
 
 console.log(3+3);
-console.log("Tirth") 
+console.log("Tirth");
+
+let name ="Tirth"
+let age = 19
+let isLoggedIn = false
+
+
+//number =>2 to power  53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => (standalone vlaue) null ek object hai
+//undefined => undefined
+//symbol => unique
+//object => {} or class
+
+
+console.log(typeof "Tirth");
+console.log(typeof null );  //object
+console.log(typeof undefined ); //undefind
