cd52c14eff81f054673d1824e3d66b418ff3a55e
Tirthchavda <tirth7859@gmail.com>
2024-12-17 12:02:26 +0530

    converstion

--- 01_basics/03_conversionOerations.js
+++ 01_basics/03_conversionOerations.js
@@ -1,4 +1,30 @@
-let score=33;
+// let score=null;
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  "33" => 33
+//  "33abc" => Nan
+// true => 1
+// false => 0
+
+let isLoggedIn = "tirth"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "tirth" => true;
+// "" => false;
+// null => false;
+// undefined => false;
+
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
