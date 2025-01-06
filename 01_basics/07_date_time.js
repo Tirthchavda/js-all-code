35410885a7acaed87fedb2402e6b7691c0e8513d
Tirthchavda <tirth7859@gmail.com>
2025-01-07 00:06:03 +0530

    dateandtime

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,27 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCretedDate = new Date(2025, 0,6)
+// let myCretedDate = new Date(2025, 0,6,5,3)
+// let myCretedDate = new Date("2025-01-06")
+// let myCretedDate = new Date("01-06-2025")
+
+// console.log(myCretedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCretedDate.getTime());
+// console.log(Date.now()/1000);
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
