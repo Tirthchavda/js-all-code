2893f8e1a583aa98102de4d9d265a3a5906ce10e
Tirthchavda <tirth7859@gmail.com>
2025-01-06 23:45:09 +0530

    math

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name="Tirth"
+const repoCount=50
+
+//console.log(name + repoCount + " value");
+
+console.log('Hello my name i ${name} and my repo count is; ${reoCount}');
---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,58 @@
+// // # primitive
+
+// // 7 type : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// const score= 100
+// const scoreValue= 100.3
+
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id===anotherId)
+
+// const bigNumber=33445354555446436n
+
+// // Refrence (Non primitive)
+
+// // Array, Objects, Functions
+
+// const hreos=["shaktiman","naahraj","doga"]
+// let myObj={
+//     "name": "Tirth",
+//     "age": 25
+// }
+
+// const myFunction=function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof myFunction);
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-primitive)
+let myYoutubname="tirth@gmail.com"
+
+let anothername = myYoutubname
+anothername = "chaiaurcode"
+
+console.log(myYoutubname);
+console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo= userOne
+
+userTwo.email="tirth@google.com"
+console.log(userOne.email);
+console.log(userTwo.email
+    
+);
